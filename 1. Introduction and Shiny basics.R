518e369669530287618bfea1548c39e7a7c3ce20
Khac Phuoc Le <phuocle@hotmail.de>
2017-12-23 15:40:13 +0100

    first commit

---
+++ 1. Introduction and Shiny basics.R
@@ -0,0 +1,55 @@
+# Welcome to the course! ####
+  
+# __ What's in an app? #####
+
+
+
+
+
+
+# __ First peek under the hood #####
+
+
+
+
+
+
+
+
+
+# User interface ####
+
+# __ Extend the UI #####
+
+
+
+
+
+
+# __ Extend the UI further #####
+
+
+
+
+
+
+
+
+
+
+# Server function ####
+
+# __ Rules of server functions #####
+
+
+
+
+
+
+# __ Fix it up #####
+
+
+
+
+
+
